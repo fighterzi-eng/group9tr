65cbed115810e3fa6493390a2dfa70ab5c63fd98
fighterzi-eng <johnbassem2@gmail.com>
2025-09-09 23:08:37 +0300

    Add files via upload
    added the launch file and modified the script

--- robotstatepub.py
+++ robotstatepub.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 #you cant publish to /tf normally you need to use a function from Transformbroadcaster from this library
 import tf
 import numpy as np
 #to read the msgs from /joint_state
 from sensor_msgs.msg import JointState
-#this library has a useful function for transforming rotation matrices to quatrenions
+#this library has a useful function for transforming homogenous transformation matrices to quatrenions
 import tf.transformations as tr
 
 
@@ -33,16 +33,34 @@ def transl(x,y,z):
                      [0,0,0,1]])
 
 
-names=[]
-positions=[]
 
 
 
 
 def callback(jobstate):
-     for i in range(0,len(jobstate.name)):
-          names[i]=jobstate.name[i]
-          positions[i]=jobstate.position[i]
+    my_dict = dict(zip(jobstate.name, jobstate.position))
+    T_base_front_left  = transl( 0.15,  0.15, -0.05) @ rot_y(my_dict["front_left_wheel_joint"])
+    T_base_front_right = transl( 0.15, -0.15, -0.05) @ rot_y(my_dict["front_right_wheel_joint"])
+    T_base_rear_left   = transl(-0.15,  0.15, -0.05) @ rot_y(my_dict["rear_left_wheel_joint"])
+    T_base_rear_right  = transl(-0.15, -0.15, -0.05) @ rot_y(my_dict["rear_right_wheel_joint"])
+    T_base_armbase= transl(0,0,0.5)
+    T_armbase_link1 = transl(0, 0, 0.1)@ rot_z(my_dict["armbase_to_link1"])
+    T_link1_link2 = transl(0, 0, 0.5)@rot_y(my_dict["link1_to_link2"])  
+    T_link1_link2=T_base_armbase@T_armbase_link1@T_link1_link2
+    T_armbase_link1=T_base_armbase@T_armbase_link1
+
+
+
+
+
+    broad.sendTransform(T_base_front_left[:3, 3] , tr.quaternion_from_matrix(T_base_front_left), rospy.Time.now(), "front_left_wheel", "base_link")
+    broad.sendTransform(T_base_front_right[:3, 3] , tr.quaternion_from_matrix(T_base_front_right), rospy.Time.now(), "front_right_wheel", "base_link")
+    broad.sendTransform(T_base_rear_left[:3, 3] , tr.quaternion_from_matrix(T_base_rear_left), rospy.Time.now(), "rear_left_wheel", "base_link")
+    broad.sendTransform(T_base_rear_right[:3, 3] , tr.quaternion_from_matrix(T_base_rear_right), rospy.Time.now(), "rear_right_wheel", "base_link")
+    broad.sendTransform(T_base_armbase[:3, 3] , [0,0,0,1], rospy.Time.now(), "armbase", "base_link")
+    broad.sendTransform(T_armbase_link1[:3, 3] , tr.quaternion_from_matrix(T_armbase_link1), rospy.Time.now(), "link1", "armbase")
+    broad.sendTransform(T_link1_link2[:3, 3] , tr.quaternion_from_matrix(T_link1_link2) ,rospy.Time.now(), "link2", "link1")
+
           
     
  
@@ -50,42 +68,5 @@ def callback(jobstate):
 rospy.init_node("robotstate")
 sub=rospy.Subscriber("/joint_states",JointState,callback)
 broad=tf.TransformBroadcaster()
-#maps every joint name to its position which eases dealing with the without confusing them
-my_dict = dict(zip(names, positions))
-#creates rotation matrices 
-#i still dont know wether to multiply it by the rotation matrix of the original oriantation of the urdf file or not
-# the @ is the multiplication of the parent with child matrices parent@child
-my_dict["front_left_wheel"]=rot_y(my_dict["front_left_wheel"])
-my_dict["rear_left_wheel"]=rot_y(my_dict["front_left_wheel"])
-my_dict["front_right_wheel"]=rot_y(my_dict["front_left_wheel"])
-my_dict["rear_right_wheel"]=rot_y(my_dict["front_left_wheel"])
-my_dict["armbase_to_link1"]=rot_z(my_dict["armbase_to_link1"])
-my_dict["link12"]=my_dict["armbase_to_link1"]@rot_y(my_dict["link12"])
-
-
-
-T_base_front_left = transl(0.15, 0.15, -0.05)
-T_base_front_right = transl(0.15, -0.15, -0.05)
-T_base_rear_left = transl(-0.15, 0.15, -0.05)
-T_base_rear_right = transl(-0.15, -0.15, -0.05)
-
-T_armbase_link1 = transl(0, 0, 0.1)
-T_link1_link2 = transl(0, 0, 0.5)  
-T_base_armbase= transl(0,0,0.5)
-T_link1_link2=T_base_armbase@T_armbase_link1@T_link1_link2
-T_armbase_link1=T_base_armbase@T_armbase_link1
-
-
-for key,val in my_dict.items():
-    q = tr.quaternion_from_matrix(my_dict[key])
-    my_dict[key]=q
-
-broad.sendTransform(T_base_front_left, my_dict["front_left_wheel"], rospy.Time.now(), "front_left_wheel", "base_link")
-broad.sendTransform(T_base_front_right, my_dict["front_right_wheel"], rospy.Time.now(), "front_right_wheel", "base_link")
-broad.sendTransform(T_base_rear_left, my_dict["rear_left_wheel"], rospy.Time.now(), "rear_left_wheel", "base_link")
-broad.sendTransform(T_base_rear_right, my_dict["rear_right_wheel"], rospy.Time.now(), "rear_right_wheel", "base_link")
-broad.sendTransform(T_armbase_link1, [0,0,0,0], rospy.Time.now(), "armbase", "base_link")
-broad.sendTransform(T_armbase_link1, my_dict["armbase_to_link1"], rospy.Time.now(), "link1", "armbase")
-broad.sendTransform(T_link1_link2, my_dict["link12"], rospy.Time.now(), "link2", "link1")
 
 rospy.spin()
